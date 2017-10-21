27af2a4fc8adcdb7334be28413d44c54db845d9b
Elliott-Greaves <greaveselliott@me.com>
2017-10-21 15:23:02 +0100

    Removed old player reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import players from '../logic/players/reducer';
 import game_play from '../logic/game-play/reducer';
 import rules from '../logic/rules/reducer';
 
 export default function createReducer() {
   return combineReducers({
-    players,
     game_play,
     rules
   });
