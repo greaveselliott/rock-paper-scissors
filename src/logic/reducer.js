1f64aaf7d075b32ed289a6f04cc9167950e77e09
Elliott-Greaves <greaveselliott@me.com>
2017-10-23 13:28:45 +0100

    Corrected reducer mutation

--- src/logic/reducer.js
+++ src/logic/reducer.js
@@ -32,43 +32,55 @@ const game_play = (state = initialState, action) => {
     });
 
     case CONSTANTS.DISPLAY_START_SCREEN:
-      return _.merge(state,{
-        game_started: true
-      });
+      return {
+        ..._.merge(state,{
+          game_started: true
+        })
+      };
 
     case CONSTANTS.COMPUTER_OR_HUMAN_PLAYER:
-      return _.merge(state,{
-        player: {
-          is_human: action.is_human
-        }
-      });
+      return {
+        ..._.merge(state,{
+          player: {
+            is_human: action.is_human
+          }
+        })
+      };
 
     case CONSTANTS.DISPLAY_SIGNAL_OPTIONS:
-      return _.merge(state,{
-        display_siganl_options: true 
-      });
+      return {
+        ..._.merge(state,{
+          display_siganl_options: true 
+        })
+      };
 
     case CONSTANTS.SELECT_SIGNAL_OPTION:
-      return _.merge(state,{
-        [action.player]: {
-          selected_symbol: action.player.is_human ? action.selected_symbol : get_random_symbol(state.ruleset)     
-        }
-      });
+      return { 
+        ..._.merge(state,{
+          [action.player]: {
+            selected_symbol: action.player.is_human ? action.selected_symbol : get_random_symbol(state.ruleset)     
+          }
+        })
+      };
 
     case CONSTANTS.REVEAL_SELECTED_SIGNAL_OPTIONS:
-      return _.merge(state,{
-        reveal_selected_signal_options: true
-      });
+      return {
+          ..._.merge(state,{
+          reveal_selected_signal_options: true
+        })
+      };
       
     case CONSTANTS.START_GAME:
-      return _.merge(state,{
-        start_game: true
-      });
+      return {
+          ..._.merge(state,{
+          start_game: true
+        })
+      };
     
     case CONSTANTS.RESTART_GAME: {
-      return _.merge(state,{
-        ...initialState
-      });
+      return {
+        ..._.merge(state,initialState)
+      };
     }
 
     case CONSTANTS.DECIDE_WINNER:
@@ -76,21 +88,27 @@ const game_play = (state = initialState, action) => {
       let opponent_symbol = state.opponent.selected_symbol;
       let outcome = calculation(player_symbol, opponent_symbol, state.ruleset);
       
-      return _.merge(state,{
-        outcome
-      });
+      return {
+          ..._.merge(state,{
+          outcome
+        })
+      };
     
     case CONSTANTS.COMPLETE_GAME:
-      return _.merge(state,{
-        complete: true
-      });
+      return {
+        ..._.merge(state,{
+          complete: true
+        })
+      };
     
     case CONSTANTS.INCREMENT_PLAYER_SCORE:
-      return _.merge(state,{
-        [action.player]: {
-          score: state[action.player].score + 1
-        }
-      });
+      return {
+          ..._.merge(state,{
+          [action.player]: {
+            score: state[action.player].score + 1
+          }
+        })
+      };
 
     default:
       return state;
