3f4df05b8bd1ad7214b3329544db3328739dfaba
Elliott-Greaves <greaveselliott@me.com>
2017-10-24 00:11:37 +0100

    Connected restart game

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -5,9 +5,9 @@ import './board.scss';
 import HandSignalSelection from '../hand-signal-selection';
 import Heading from '../heading';
 import Button from '../button';
-import { decide_winner } from '../../logic/actions';
+import { decide_winner, restart_game } from '../../logic/actions';
 
-const Board = ({className, active, hand_signals, selected_player_symbol, selected_opponent_symbol, is_revealing, outcome, on_decide_winner}) => {
+const Board = ({className, active, hand_signals, selected_player_symbol, selected_opponent_symbol, is_revealing, outcome, on_decide_winner, on_reset_game}) => {
     return (
         <div className={`${className}${active ? "--inactive": ""} l-board`}>
             <div className={`l-board__opponent${enter_exit_modifier(is_revealing)}`}>
@@ -23,7 +23,7 @@ const Board = ({className, active, hand_signals, selected_player_symbol, selecte
                 <div className={`l-board__select-symbol${enter_exit_modifier(is_revealing)}`}><Heading title="Select a symbol."/></div>
                 <div className={`l-board__outcome${enter_exit_modifier(is_revealing === false)}`}>
                     <Heading title={`You ${outcome}!`}/>
-                    <Button modifier="--large" text="Restart game"/>
+                    <Button click_handler={on_reset_game} modifier="--large" text="Restart game"/>
                 </div>
             </div>
             <div className={`l-board__player${enter_exit_modifier(is_revealing)}`}>
@@ -54,7 +54,8 @@ Board.propTypes = {
 };
 
 const mapDispatchToProps = dispatch => ({
-    on_decide_winner: () => dispatch(decide_winner())
+    on_decide_winner: () => dispatch(decide_winner()),
+    on_reset_game: () => dispatch(restart_game())
 });
 
 const mapStateToProps = state => {
